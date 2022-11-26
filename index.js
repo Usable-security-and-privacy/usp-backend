02f1edc32e9d50a121533bed9b665236bd6f8504
Neeraj Gartia <neerajgartia211002@gmail.com>
2022-11-26 20:29:08 +0530

    added admin dashboard

--- index.js
+++ index.js
@@ -2,13 +2,28 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 require('dotenv').config()
-require('./config/mongoose');
-
+const mongoose=require('./config/mongoose');
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const AdminJS = require('adminjs');
+const AdminJSExpress = require('@adminjs/express');
+
 const Data=require('./models/data');
+
+const AdminJSMongoose = require("@adminjs/mongoose");
+
+AdminJS.registerAdapter(AdminJSMongoose)
+
+const admin = new AdminJS({
+    rootPath: '/admin',
+    resources: [
+        Data, // you can simply pass a model
+    ]
+})
+const adminRouter = AdminJSExpress.buildRouter(admin)
+app.use(admin.options.rootPath, adminRouter)
 
 app.post('/api/submit', async (req, res) => {
     try {
@@ -25,4 +40,8 @@ app.post('/api/submit', async (req, res) => {
             message: error.message
         });
     }
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
